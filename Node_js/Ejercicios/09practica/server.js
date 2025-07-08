323a3c4d6525bb5fd2599802202a64b3f8655e09
Edson-Moreno1 <edson.morenoortega@gmail.com>
2025-07-08 22:00:22 +0000

    Update 2 mongodb

--- Node_js/Ejercicios/09practica/server.js
+++ Node_js/Ejercicios/09practica/server.js
@@ -1,5 +1,7 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 
-const user = require('./src/models/user');
+const uri = 'mongodb+srv://edsonmorenoortega:Recuerdame01@cluster0.80tvxfm.mongodb.net/tu_base?retryWrites=true&w=majority&appName=Cluster0';
 
-mongoose.connect('')
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('✅ Conectado a MongoDB Atlas!'))
+  .catch(err => console.error('❌ Error al conectar a MongoDB:', err));
