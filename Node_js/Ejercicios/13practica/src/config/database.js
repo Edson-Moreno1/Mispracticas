5b25a5e44cafdcde5817f39b20c15381e6efa9ed
Edson-Moreno1 <edson.morenoortega@gmail.com>
2025-08-05 19:53:44 -0600

    update

--- Node_js/Ejercicios/13practica/src/config/database.js
+++ Node_js/Ejercicios/13practica/src/config/database.js
@@ -1,44 +1,48 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 
 dotenv.config();
 
-async function ensureDataBaseExist() {
-
-    
+async function ensureDatabaseExists() {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS || undefined,
+  });
+  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`);
+  await connection.end();
 }
 
-
-await ensureDataBaseExist();
+await ensureDatabaseExists();
 
 const sequelize = new Sequelize(
-    '13practica_ORM',
-    'root',
-    'password',
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-        logging: false,
-        pool:{
-            max:5,
-            min:0,
-            acquire:30000,
-            idle:100000,
-        }
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS || null, // Usa null si no hay contraseña
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 100000,
     }
+  }
 );
 
-async function testConnection(){
-    try {
-        await sequelize.authenticate();
-        await sequelize.sync();
-        console.log('Connection has been established successfully.');
-    }catch (error){
-        console.error('Cannot be connected to  Mysql');
-    }
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    console.log('Connection test');
+  } catch (error) {
+    console.error('Cannot be connected to Mysql');
+  }
 }
 
 testConnection();
 
-export {sequelize};
+export { sequelize };
